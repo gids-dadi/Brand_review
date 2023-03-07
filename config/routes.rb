1572deb1e14c429b33ece5404b73b39d93493a8d
Gideon <gideons564@gmail.com>
2023-03-07 10:34:22 +0100

    Create the dev branch for kit flow

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get '*page', to: 'static#index', constraints: ->(req) do
     !req.xhr? && req.format.html?
   end
-  root "static#index"
+  root "hello_react#index"
 end
